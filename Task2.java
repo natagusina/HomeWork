f864fa2f4d5d23051b2bed4db83491392d213bd5
natagusina <natagusina@gmail.com>
2023-01-15 02:15:26 +0100

    1v

---
+++ Task2.java
@@ -0,0 +1,35 @@
+package HomeWork8;
+import java.util.Scanner;
+public class Task2 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 10; i++) {
+
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Would you like to apply for a credit card?");
+            String answer = scan.next();
+            if(answer.equalsIgnoreCase("yes")){
+                break;
+            }
+
+        }
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+
+
---
+++ Task5.java
@@ -0,0 +1,12 @@
+package HomeWork8;
+
+public class Task5 {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int o=i;o<=5;o++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
